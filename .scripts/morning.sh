74fe90550e62572d0975d90f72b6a3dcd69a82f0
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2021-01-24 13:00:41 +0100

    Updates

--- .scripts/morning.sh
+++ .scripts/morning.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 echo "Good Morning Nik!"
 
+# Update Homebrew
 echo "Updating Homebrew..."
 brew update
 
+# Update dotfiles
+# TODO Import alias?
 echo "Pushing current state of dotfiles-apple..."
 alias df='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias dfp="df commit -a -m \"Updates\" && df push"
 dfp
 
-# TODO Import alias?
+#  Update stoic theme
+sh ~/Documents/notes/getStoicTheme.sh
+
