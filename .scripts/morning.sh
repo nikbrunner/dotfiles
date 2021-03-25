fd6f69aab62f353642e436b78ca558cecee117da
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2021-03-25 20:19:34 +0100

    Updates

--- .scripts/morning.sh
+++ .scripts/morning.sh
@@ -14,6 +14,6 @@ dfp
 
 #  Update stoic theme
 echo ": Updating Obsidian Stoic Theme"
-sh $HOME/Library/Mobile Documents/iCloud~md~obsidian/Documents/Notezz/getStoicTheme.sh
-sh $HOME/pCloud Drive/Dokumente/nik/work/dev/dev-notes/getStoicTheme.sh
+sh $HOME/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/Notezz/getStoicTheme.sh
+sh $HOME/pCloud\ Drive/Dokumente/nik/work/dev/dev-notes/getStoicTheme.sh
 
