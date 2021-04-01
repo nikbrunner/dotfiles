2c91403e2c0b3c816c5844f7ad9c14c257ce4a42
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2021-04-01 22:34:09 +0200

    "Updates"

--- .scripts/morning.sh
+++ .scripts/morning.sh
@@ -10,10 +10,11 @@ brew upgrade
 # TODO Import alias?
 echo ": Pushing current state of dotfiles-apple..."
 alias df='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-alias dfp="df commit -a -m \"Updates\" && df push"
-dfp
+# alias dfp="df commit -a -m \"Updates\" && df push"
+df commit -a -m \"Updates\" && df push
 
 # Update vim config
+echo ": Pushing current state of neovim config..."
 NVIM_PATH="$HOME/.config/nvim"
 git -C $NVIM_PATH commit -a -m \"Updates\" && git -C $NVIM_PATH push
 
