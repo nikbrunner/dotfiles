b4d552f97bd5b50f674039305b5d15905da6705f
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2021-04-01 22:31:47 +0200

    Updates

--- .scripts/morning.sh
+++ .scripts/morning.sh
@@ -13,6 +13,10 @@ alias df='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias dfp="df commit -a -m \"Updates\" && df push"
 dfp
 
+# Update vim config
+NVIM_PATH="$HOME/.config/nvim"
+git -C NVIM_PATH commit -a -m \"Updates\" && git -C NVIM_PATH push
+
 #  Update stoic theme
 echo ": Updating Obsidian Stoic Theme"
 sh $HOME/.scripts/updateStoicTheme.sh
