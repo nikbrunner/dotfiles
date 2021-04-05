7236faa54ecf8c6fec4dba55eb65fee0c4dc0a27
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2021-04-05 21:37:14 +0200

    "Updates"

--- .scripts/morning.sh
+++ .scripts/morning.sh
@@ -6,12 +6,6 @@ NC='\033[0m' # No Color
 
 echo "${GREEN}Good Morning Nik!${NC}"
 
-# Update Homebrew
-echo "${ORANGE}::: Updating Homebrew...${NC}"
-brew update
-brew outdated
-# brew upgrade
-
 # Update vim config
 echo "${ORANGE}::: Updating NeoVim config...${NC}"
 NVIM_PATH="$HOME/.config/nvim"
@@ -26,4 +20,14 @@ df commit -a -m \"Updates\" && df push
 #  Update stoic theme
 echo "${ORANGE}::: Updating Obsidian Stoic Theme${NC}"
 sh $HOME/.scripts/updateStoicTheme.sh
+
+# Update Homebrew
+echo "${ORANGE}::: Updating Homebrew...${NC}"
+brew update
+brew outdated
+# brew upgrade
+
+if [ "$1" = 'full' ]; then
+    echo "fulll jo"
+fi
 
