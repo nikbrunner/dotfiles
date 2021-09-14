3243756c302cafd8ad80dbacf0c2314b2e6484f1
nibru <nikolaus.brunner@protonmail.ch>
2021-09-14 10:00:42 +0200

    "Updates"

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,26 +1,38 @@
---[[
-lvim is the global options object
+-- TODO
+-- Tab for switching Buffers instead of H and L
+-- H and L for ^ and $
 
-Linters should be
-filled in as strings with either
-a global executable or a path to
-an executable
-]]
--- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
 
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
 lvim.colorscheme = "github"
 
+-- This is how to create a submenu
+-- lvim.builtin.which_key.mappings["f"] = {
+--   name = "Find",
+--   f = { "<cmd>Telescope find_files<CR>", "File" },
+--   g = { "<cmd>Telescope git_status<CR>", "Git Status" },
+-- }
+
+-- Add Telescope git_status to git submenu
+lvim.builtin.which_key.mappings["gm"] = {
+  "<cmd>Telescope git_status<CR>", "Modified Files",
+}
+
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
--- add your own keymapping
-lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+
 -- unmap a default keymapping
 -- lvim.keys.normal_mode["<C-Up>"] = ""
+
 -- edit a default keymapping
--- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
+
+-- Go to the beginning and end of a line with H and L
+lvim.keys.normal_mode["<S-h>"] = "^"
+lvim.keys.normal_mode["<S-l>"] = "$"
+lvim.keys.normal_mode["<S-Tab>"] = ":BufferPrevious<CR>"
+lvim.keys.normal_mode["<Tab>"] = ":BufferNext<CR>"
 
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
 -- lvim.builtin.telescope.on_config_done = function()
@@ -60,42 +72,14 @@ lvim.builtin.treesitter.ensure_installed = "maintained"
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
--- formater
+-- formater typescript
 lvim.lang.typescript.formatters = { { exe = "prettier" } }
 lvim.lang.typescriptreact.formatters = lvim.lang.typescript.formatters
 
--- generic LSP settings
--- you can set a custom on_attach function that will be used for all the language servers
--- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
--- lvim.lsp.on_attach_callback = function(client, bufnr)
---   local function buf_set_option(...)
---     vim.api.nvim_buf_set_option(bufnr, ...)
---   end
---   --Enable completion triggered by <c-x><c-o>
---   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
--- end
--- you can overwrite the null_ls setup table (useful for setting the root_dir function)
--- lvim.lsp.null_ls.setup = {
---   root_dir = require("lspconfig").util.root_pattern("Makefile", ".git", "node_modules"),
--- }
--- or if you need something more advanced
--- lvim.lsp.null_ls.setup.root_dir = function(fname)
---   if vim.bo.filetype == "javascript" then
---     return require("lspconfig/util").root_pattern("Makefile", ".git", "node_modules")(fname)
---       or require("lspconfig/util").path.dirname(fname)
---   elseif vim.bo.filetype == "php" then
---     return require("lspconfig/util").root_pattern("Makefile", ".git", "composer.json")(fname) or vim.fn.getcwd()
---   else
---     return require("lspconfig/util").root_pattern("Makefile", ".git")(fname) or require("lspconfig/util").path.dirname(fname)
---   end
--- end
+-- formater javascript
+lvim.lang.javascript.formatters = { { exe = "prettier" } }
+lvim.lang.javascriptreact.formatters = lvim.lang.typescript.formatters
 
--- set a formatter if you want to override the default lsp one (if it exists)
--- lvim.lang.python.formatters = {
---   {
---     exe = "black",
---   }
--- }
 -- set an additional linter
 -- lvim.lang.python.linters = {
 --   {
@@ -110,16 +94,16 @@ lvim.plugins = {
     --   "folke/trouble.nvim",
     --   cmd = "TroubleToggle",
     -- },
-    { 'projekt0n/github-nvim-theme' }
+  { 
+    'projekt0n/github-nvim-theme',
+    config = function()
+      require('github-theme').setup({
+        themeStyle = "dark",
+        transparent = true
+      })
+    end,
+  }
 }
-
--- Lua
-require('github-theme').setup({
-    themeStyle = "dark",
-    transparent = true
-})
-
-
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- lvim.autocommands.custom_groups = {
