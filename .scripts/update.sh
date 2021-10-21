7b3aeac3db5990521318a9510dd3dbd7ab67ea74
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2021-10-21 06:40:03 +0200

    "Updates"

--- .scripts/update.sh
+++ .scripts/update.sh
@@ -7,58 +7,59 @@ NC='\033[0m' # No Color
 echo -e "${GREEN}Good Morning Nik!${NC}"
 # Update vim config
 function updateVimDotfiles () {
-    echo -e "${ORANGE}::: Updating NeoVim dotfiles...${NC}"
-    NVIM_PATH="$HOME/.config/nvim"
-    git -C $NVIM_PATH pull --rebase
-    git -C $NVIM_PATH add .
-    git -C $NVIM_PATH commit -a -m \"Updates\" && git -C $NVIM_PATH push
+  echo -e "${ORANGE}::: Updating NeoVim dotfiles...${NC}"
+
+  NVIM_PATH="$HOME/.config/nvim"
+
+  git -C $NVIM_PATH pull --rebase
 }
 
-function homeDirGit () {
-    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $1
-} 
 
 # Update dotfiles
 function updateDotfiles () {
-    echo -e "${ORANGE}::: Updating Home-Directory dotfiles...${NC}"
-    DOTS_PATH="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
-    git -C $DOTS_PATH pull --rebase
-    git -C $DOTS_PATH commit -a -m "Updates"
-    git -C $DOTS_PATH push
+  echo -e "${ORANGE}::: Updating Home-Directory dotfiles...${NC}"
+
+  function homeDirGit () {
+    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $1
+  } 
+
+  homeDirGit stash
+  homeDirGit pull --rebase
+  homeDirGit stash pop
 }
 
 # Update Homebrew
 function updateHomebrew () {
-    echo -e "${ORANGE}::: Updating Homebrew...${NC}"
-    brew update
-    brew outdated
+  echo -e "${ORANGE}::: Updating Homebrew...${NC}"
+  brew update
+  brew outdated
 }
 
 function updateFull () {
-    updateVimDotfiles
-    updateDotfiles
-    updateHomebrew
+  updateVimDotfiles
+  updateDotfiles
+  updateHomebrew
 }
 
 case $1 in
-    "--dots")
-        updateDotfiles
+  "--dots")
+    updateDotfiles
     ;;
 
-    "--nv")
-        updateVimDotfiles
+  "--nv")
+    updateVimDotfiles
     ;;
 
-    "--brew")
-        updateHomebrew
+  "--brew")
+    updateHomebrew
     ;;
 
-    "--full")
-        updateFull
+  "--full")
+    updateFull
     ;;
 
-    *)
-        echo "Unknown argument \"$1\""
+  *)
+    echo "Unknown argument \"$1\""
     ;;
 esac
 
